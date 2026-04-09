80fae6624220beda4a315992c510522f23f1c63d
Nicolas Adler <114955515+NicolasAdler@users.noreply.github.com>
2026-04-09 16:44:37 -0700

    Refactor output format and update filename

--- main.cpp
+++ main.cpp
@@ -4,35 +4,45 @@
 
 int main()
 {
-    std::string filename = "//Users/nicolasadler/Documents/2026/rockford/Speaker_SPL_Calculator/sample_speaker.txt";
+    std::string filename = "sample_speaker.txt";
     std::ifstream inFile(filename);
 
     TS_Parameters speaker1;
     speaker1.initialize_speaker(inFile);
-    //speaker1.set_Mms(speaker1.get_fs(), speaker1.get_Cms());
     speaker1.solve();
-    std::cout<<speaker1.get_Vas()<<std::endl;
-    std::cout<<speaker1.get_fs()<<std::endl;
-    std::cout<<speaker1.get_Qes()<<std::endl;
-    std::cout<<speaker1.get_Qms()<<std::endl;
-    std::cout<<speaker1.get_Xmax()<<std::endl;
-    std::cout<<speaker1.get_Sd()<<std::endl;
-    std::cout<<speaker1.get_Sensitivity()<<std::endl;
-    std::cout<<speaker1.get_Cms()<<std::endl;
+    std::cout<<"Vas: "<<speaker1.get_Vas()<<std::endl;
+    std::cout<<"fs: "<<speaker1.get_fs()<<std::endl;
+    std::cout<<"Qts: "<<speaker1.get_Qts()<<std::endl;
+    std::cout<<"Qes: "<<speaker1.get_Qes()<<std::endl;
+    std::cout<<"Qms: "<<speaker1.get_Qms()<<std::endl;
+    std::cout<<"Xmax: "<<speaker1.get_Xmax()<<std::endl;
+    std::cout<<"Sd: "<<speaker1.get_Sd()<<std::endl;
+    std::cout<<"Sensitivity: "<<speaker1.get_Sensitivity()<<std::endl;
+    std::cout<<"Cms: "<<speaker1.get_Cms()<<std::endl;
+    std::cout<<"Mms: "<<speaker1.get_Mms()<<std::endl;
+    std::cout<<"Rms: "<<speaker1.get_Rms()<<std::endl;
+    std::cout<<"Bl: "<<speaker1.get_Bl()<<std::endl;
     
     /*
      CORRECT OUTPUT:
-     5.4
-     42.4
-     1.6
-     9.108
-     3.6
-     219
-     77.54
-     0.08
+	Vas 5.4
+	fs 42.4
+	Qts 1.361
+	Qes 1.6
+	Qms 9.108
+	Xmax 3.6
+	Sd 219
+	Sensitivity 77.54
+	Re 0.75
+	n0 0.00025
+	Cms 0.08
+	Mms 175.75
+	Rms 5.14
+	Bl 4.68
      
      */
-    //FS NOT WORKING
+    // CMS NOT WORKING
+    // VAS NOT WORKING
 
     return 0;
 }
